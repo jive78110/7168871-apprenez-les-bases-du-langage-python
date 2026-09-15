4bb6ed998384fe6bac1857b5d28f88bca20fcad9
jive78110 <167408954+jive78110@users.noreply.github.com>
2026-09-15 09:24:22 +0200

    Delete P2 directory

--- P2/P2C2/énoncé/main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    # Ecrivez votre code ici !
-    # Attention tout votre code doit être indenté comme ce commentaire
-
-
-# Ne touchez pas le code ci-dessous
-if __name__ == "__main__":
-    main()
--- P2/P2C3/énoncé/main.py
+++
@@ -1,5 +0,0 @@
-# Ecrivez vos fonctions ici
-
-# Ne touchez pas le code ci-dessous
-if __name__ == "__main__":
-    main()
